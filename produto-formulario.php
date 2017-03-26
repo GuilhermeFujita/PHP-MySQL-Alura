16b929c021df49f457bcffb4715ece8c799d7b47
GuilhermeFujita <fujitaguilherme@gmail.com>
2017-03-26 16:26:42 -0300

    Arquivos aulas 1 a 6 - Deleção de produtos

---
+++ produto-formulario.php
@@ -0,0 +1,19 @@
+<?php include("cabecalho.php");?>
+
+	
+		<form action="adiciona-produto.php">
+			<table class="table">
+				<tr>
+					<td>Nome:</td>
+					<td><input class="form-control" type="text" name="nome"></td>
+				</tr>
+				<tr>
+					<td>Preço:</td>
+					<td><input class="form-control" type="number" name="preco"></td>
+				</tr>
+				<tr>
+					<td><input class="btn btn-primary" type="submit" value="Cadastrar"></td>
+				</tr>
+			</table>
+		</form>
+<?php include("rodape.php");?>
