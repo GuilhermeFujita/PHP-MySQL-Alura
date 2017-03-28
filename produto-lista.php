60b0ec7ed5e2e70b804312052c8c665927247c78
GuilhermeFujita <fujitaguilherme@gmail.com>
2017-03-28 14:30:06 -0300

    Aula 07- Utilizando o método POST

--- produto-lista.php
+++ produto-lista.php
@@ -14,8 +14,12 @@ $produtos = listaProdutos($conexao);
 	<tr>
 		<td><?=$produto['nome']?></td>
 		<td><?=$produto['preco']?></td>
+		<td><?=substr($produto['descricao'],0,40)?></td>
 		<td>
-			<a href="remove-produto.php?id=<?=$produto['id']?>" class="text-danger">Remover</a>
+			<form action="remove-produto.php" method="post">
+				<input type="hidden" name="id" value="<?=$produto['id']?>">
+				<button class="btn btn-danger">Remover</button>
+			</form>
 		</td>
 	</tr>
 	<?php
